b7297f522b5e94591fce53b5b731a7973a1baf43
Alex Mark <alexvmar@yandex.ru>
2023-02-09 22:28:31 +0300

    corrected file8_1

--- Example008_Dog/Program.cs
+++ Example008_Dog/Program.cs
@@ -3,28 +3,27 @@ int velocityMen_2 = 2;
 int velocityDog = 5;
 double distance = 10000;
 int stopProg = 10;
-int step = 0;
+int step = 1;
 double time = distance / (velocityDog - velocityMen_1);
 double distanceMen_2 = time * velocityMen_2;
 double distanceMen_1 = time * velocityMen_1;
 distance = distance + distanceMen_1 - distanceMen_2;
-step++;
 
 while (distance > stopProg)
 {
-    step++;
     time = distance / (velocityDog + velocityMen_2);
     distanceMen_2 = time * velocityMen_2;
     distanceMen_1 = time * velocityMen_1;
     distance = distance + distanceMen_1 - distanceMen_2;
+    step++;
 
     if (distance > stopProg)
     {
-        step++;
         time = distance / (velocityDog - velocityMen_1);
         distanceMen_2 = time * velocityMen_2;
         distanceMen_1 = time * velocityMen_1;
         distance = distance + distanceMen_1 - distanceMen_2;
+        step++;
 
     }
 }
