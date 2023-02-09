3bfd274f987bb5e4e08b35bfec77cb43d4d4d779
Alex Mark <alexvmar@yandex.ru>
2023-02-09 15:59:35 +0300

    corrected file2

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введи своё имя, ");
+﻿Console.Write("Введите своё имя: ");
 string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.WriteLine(username);
+Console.Write("Вас зовут: ");
+Console.Write(username);
