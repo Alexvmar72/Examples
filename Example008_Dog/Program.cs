1a5e26d5289e771192f7837eecdcf8a69619e68b
Alex Mark <alexvmar@yandex.ru>
2023-02-09 21:21:36 +0300

    corrected file8

--- Example008_Dog/Program.cs
+++ Example008_Dog/Program.cs
@@ -7,25 +7,27 @@ int step = 0;
 double time = distance / (velocityDog - velocityMen_1);
 double distanceMen_2 = time * velocityMen_2;
 double distanceMen_1 = time * velocityMen_1;
-double distanceStop = distance + distanceMen_1 - distanceMen_2;
+distance = distance + distanceMen_1 - distanceMen_2;
 
-while (distanceStop > stopProg)
+while (distance > stopProg)
 {
-    step++;
+    step = step + 1;
     time = distance / (velocityDog + velocityMen_2);
     distanceMen_2 = time * velocityMen_2;
     distanceMen_1 = time * velocityMen_1;
-    distanceStop = distance + distanceMen_1 - distanceMen_2;
-    if (distanceStop > stopProg)
+    distance = distance + distanceMen_1 - distanceMen_2;
+
+    if (distance > stopProg)
     {
         step++;
         time = distance / (velocityDog - velocityMen_1);
         distanceMen_2 = time * velocityMen_2;
         distanceMen_1 = time * velocityMen_1;
-        distanceStop = distance + distanceMen_1 - distanceMen_2;
+        distance = distance + distanceMen_1 - distanceMen_2;
 
     }
 }
 
 Console.Write("Собака пробежит от одного к другому: ");
-Console.WriteLine(step);
+Console.Write(step);
+Console.WriteLine("раз.");
